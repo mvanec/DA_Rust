229682bfd7789e2075b12f3e53f07556665de813
mvanec <matt@mattvanecek.com>
2024-06-07 19:04:46 -0500

    Step 5: Synced B over A

--- trades_a/src/main.rs
+++ trades_a/src/main.rs
@@ -33,7 +33,8 @@ impl fmt::Display for Trade {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // let csv_data_loader = CsvDataLoader::new(
     //     "W:\\DataAnnotation\\Rust\\test_data\\trades.csv".to_string(),
     //     "W:\\DataAnnotation\\Rust\\test_data\\trade_executions.csv".to_string(),
@@ -46,10 +47,15 @@ fn main() {
         "data".to_string(),
         "dataannotation".to_string(),
         "dataannotation".to_string(),
-    );
+    ).await;
 
-    let trade_factory = TradeFactory::new(Box::new(mysql_data_loader));
-    let trades = trade_factory.load_trades();
+    // let trade_factory = TradeFactory::new(Box::new(mysql_data_loader));
+    // let trades = trade_factory.load_trades();
+
+    // for trade in trades {
+    //     println!("{}", trade);
+    // }
+    let trades = mysql_data_loader.load_trades().await;
 
     for trade in trades {
         println!("{}", trade);
--- trades_a/src/mysql_data_loader.rs
+++ trades_a/src/mysql_data_loader.rs
@@ -1,28 +1,26 @@
 // mysql_data_loader.rs
 use crate::data_loader::DataLoader;
 use crate::models::*;
-use mysql::prelude::*;
+use mysql_async::{Opts, Pool};
+use mysql_async::prelude::*;
 
 pub struct MySqlDataLoader {
-    pool: mysql::Pool,
+    pool: Pool,
 }
 
 impl MySqlDataLoader {
-    pub fn new(url: String, user: String, password: String, db: String) -> Self {
+    pub async fn new(url: String, user: String, password: String, db: String) -> Self {
         let url = format!("mysql://{}:{}@{}/{}", user, password, url, db);
-        // println!("URL: {}", url);
-        let opts = mysql::Opts::from_url(&url).unwrap();
-        let pool = mysql::Pool::new(opts).unwrap();
+        let opts = Opts::from_url(&url).unwrap();
+        let pool = Pool::new(opts);
         Self { pool }
     }
-}
 
-impl DataLoader for MySqlDataLoader {
-    fn load_trades(&self) -> Vec<Trade> {
-        let mut conn = self.pool.get_conn().unwrap();
+    pub async fn load_trades(&self) -> Vec<Trade> {
+        let mut conn = self.pool.get_conn().await.unwrap();
 
         let mut trades: Vec<Trade> = conn.query_map(
-            "SELECT TradeID, Symbol, OpenDate, CloseDate, BrokerID, ExchangeID, RealizedGain FROM trades",
+            "SELECT TradeID, Symbol, OpenDate, CloseDate, BrokerID, ExchangeID, RealizedGain FROM Trades",
             |(trade_id, symbol, open_date, close_date, broker_id, exchange_id, realized_gain)| Trade {
                 trade_id,
                 symbol,
@@ -33,10 +31,10 @@ impl DataLoader for MySqlDataLoader {
                 realized_gain,
                 executions: Vec::new(),
             },
-        ).unwrap();
+        ).await.unwrap();
 
-        let trade_executions: Vec<TradeExecution> = conn.query_map(
-            "SELECT ExecutionID, TradeID, ExecutionDateTime, Spread, Quantity, PositionEffect, OrderPrice, FillPrice, Commission, Fees, ReferenceNumber FROM trade_executions",
+        let mut trade_executions: Vec<TradeExecution> = conn.query_map(
+            "SELECT ExecutionID, TradeID, ExecutionDateTime, Spread, Quantity, PositionEffect, OrderPrice, FillPrice, Commission, Fees, ReferenceNumber FROM TradeExecutions",
             |(execution_id, trade_id, execution_date_time, spread, quantity, position_effect, order_price, fill_price, commission, fees, reference_number)| TradeExecution {
                 execution_id,
                 trade_id,
@@ -51,10 +49,10 @@ impl DataLoader for MySqlDataLoader {
                 reference_number,
                 options: Vec::new(),
             },
-        ).unwrap();
+        ).await.unwrap();
 
-        let options_details: Vec<OptionDetail> = conn.query_map(
-            "SELECT OptionID, ExecutionID, Expiration, Strike, Type, Quantity, Premium, Opra FROM options_details",
+        let mut options_details: Vec<OptionDetail> = conn.query_map(
+            "SELECT OptionID, ExecutionID, Expiration, Strike, Type, Quantity, Premium, Opra FROM OptionsDetails",
             |(option_id, execution_id, expiration, strike, option_type, quantity, premium, opra)| OptionDetail {
                 option_id,
                 execution_id,
@@ -65,7 +63,7 @@ impl DataLoader for MySqlDataLoader {
                 premium,
                 opra,
             },
-        ).unwrap();
+        ).await.unwrap();
 
         for trade in &mut trades {
             trade.executions = trade_executions
