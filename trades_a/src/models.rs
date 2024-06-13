873dc266be807d434ad8add250854286793f8d2b
mvanec <matt@mattvanecek.com>
2024-06-13 15:13:10 -0500

    Refactoring before the next Task

--- trades_a/src/models.rs
+++ trades_a/src/models.rs
@@ -1,5 +1,6 @@
 // models.rs
 use serde::Deserialize;
+use std::fmt;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Trade {
@@ -67,4 +68,35 @@ pub struct OptionDetail {
     pub premium: f64,
     #[serde(rename = "Opra")]
     pub opra: String,
+}
+
+impl fmt::Display for Trade {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(
+            f,
+            "| {:^5} | {:^6} | {:^10} | {:^5} | {:^5} | {:^10} |",
+            self.trade_id, self.symbol, self.open_date, self.broker_id,
+            self.exchange_id, self.realized_gain.unwrap_or(0.0)
+        )?;
+
+        for execution in &self.executions {
+            writeln!(
+                f,
+                "    | {:^5} | {:^19} | {:^6} | {:^6} | {:^10} | {:^10} |",
+                execution.execution_id, execution.execution_date_time, execution.quantity,
+                execution.order_price, execution.commission, execution.fees
+            )?;
+
+            for option in &execution.options {
+                writeln!(
+                    f,
+                    "        | {:^5} | {:^10} | {:^6} | {:^10} | {:^5} | {:^10} | {:^20} |",
+                    option.option_id, option.expiration, option.quantity, option.strike,
+                    option.option_type, option.premium,option.opra
+                )?;
+            }
+        }
+
+        Ok(())
+    }
 }
